0aacab26d977a89a0930093395f232ce3683538c
WictorHiago <wictor.hsc@gmail.com>
2024-04-21 01:11:41 -0400

    feat: create post for publish comments, hotfix layout

---
+++ app/(main)/page.tsx
@@ -0,0 +1,18 @@
+import Post from "../components/post/post";
+
+export default function Main() {
+  return (
+    <div>
+      <Post />
+      <h1>Nossa primeira pagina</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim tempora nemo suscipit. Sequi enim soluta laborum.
+        Amet cum vero est, accusantium ipsa hic, eaque vitae quasi et error ducimus. Accusantium! Lorem ipsum dolor sit
+        amet consectetur, adipisicing elit. Aperiam itaque et animi explicabo non nemo, officiis reiciendis consequatur
+        voluptates? Debitis quo labore, quod at ad beatae perferendis nihil eos iure. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Et eius adipisci sed quis modi rem atque error dolores in tempora illum odio cum
+        harum doloremque aut, molestiae est asperiores! Deserunt?
+      </p>
+    </div>
+  );
+}
